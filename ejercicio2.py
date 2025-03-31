29be126fbde9d9411e040e668db068efab74f34d
nicolasmdz <nicolasmonacoojeda@gmail.com>
2025-03-30 22:02:59 -0300

    se modifico ejercicio3

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,5 +1,5 @@
-nombre="Nicolas"
-apellido="Monaco"
+nombre=input("Como te llamas?")
+apellido=input("Cual es tu apellido?")
 materia="Programacion1"
 saludo=(f"Soy {nombre} {apellido} profesor de {materia}")
 print(saludo)
