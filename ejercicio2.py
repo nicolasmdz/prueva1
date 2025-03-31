0985c803b474fc677f8f2de38329ad0b737054ec
nicolasmdz <nicolasmonacoojeda@gmail.com>
2025-03-30 22:13:21 -0300

    se agregan comentarios a ejercicio2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,5 +1,9 @@
+# Se define variable nombre
 nombre="Nicolas"
+# se define variable apellido
 apellido="Monaco"
+# se define variable materia
 materia="Programacion1"
 saludo=(f"Soy {nombre} {apellido} profesor de {materia}")
+# Se imprime saludo por consola
 print(saludo)
--- ejercicio3.py
+++
@@ -1,5 +0,0 @@
-nombre=input("Como te llamas?")
-apellido=input("Cual es tu apellido?")
-materia="Programacion1"
-saludo=(f"Soy {nombre} {apellido} profesor de {materia}")
-print(saludo)
